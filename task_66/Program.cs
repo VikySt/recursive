4c494bc0962cc0954c71c278222b1175c8f31658
Viktoria <vikystrelnikova@yandex.ru>
2022-11-17 07:25:31 +0500

    task_66

---
+++ task_66/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// (НЕ ИСПОЛЬЗОВАТЬ РЕЗУЛЬТИРУЮЩУЮ ПЕРЕМЕННУЮ)
+
+int RecursiveSum(int m, int n)
+{
+    if (m == n)
+        return m;
+
+    return m + RecursiveSum(m + 1, n);
+}
+
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(RecursiveSum(m, n));
